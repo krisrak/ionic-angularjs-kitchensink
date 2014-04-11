810e3dcb3f0550facbe429e320d8d9ad0a08f2d6
Rakshith Krishnappa <rakshith.krishnappa@intel.com>
2014-04-11 15:03:07 -0700

    adding kitchensink app

---
+++ js/app.js
@@ -0,0 +1,206 @@
+angular.module('ionicApp', ['ionic'])
+
+.config(function ($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('menu', {
+      url: "/menu",
+      abstract: true,
+      templateUrl: "menu.html",
+      controller: 'MenuCtrl'
+    })
+    .state('menu.tabs', {
+      url: "/tab",
+      views: {
+        'menuContent' :{
+          templateUrl: "tabs.html"
+        }
+      }
+    })
+    .state('menu.tabs.buttons', {
+      url: "/buttons",
+      views: {
+        'buttons-tab': {
+          templateUrl: "buttons.html",
+          controller: 'ButtonsTabCtrl'
+        }
+      }
+    })
+    .state('menu.tabs.list', {
+      url: "/list",
+      views: {
+        'list-tab': {
+          templateUrl: "list.html",
+          controller: 'ListCtrl'
+        }
+      }
+    })
+    .state('menu.tabs.item', {
+      url: "/item",
+      views: {
+        'list-tab': {
+          templateUrl: "item.html"
+        }
+      }
+    })
+    .state('menu.tabs.form', {
+      url: "/form",
+      views: {
+        'form-tab': {
+          templateUrl: "form.html"
+        }
+      }
+    })
+    .state('menu.keyboard', {
+      url: "/keyboard",
+      views: {
+        'menuContent': {
+          templateUrl: "keyboard.html"
+        }
+      }
+    })
+    .state('menu.slidebox', {
+      url: "/slidebox",
+      views: {
+        'menuContent': {
+          templateUrl: "slidebox.html",
+          controller: 'SlideboxCtrl'
+        }
+      }
+    })
+    .state('menu.about', {
+      url: "/about",
+      views: {
+        'menuContent': {
+          templateUrl: "about.html"
+        }
+      }
+    });
+
+  $urlRouterProvider.otherwise("menu/tab/buttons");
+
+})
+
+.controller('ListCtrl', function ($scope) {
+
+  $scope.data = {
+    showDelete: false
+  };
+
+  $scope.itemButtons = [
+    {
+      text: 'Delete',
+      type: 'button-assertive',
+      onTap: function (item) {
+        alert('Delete Item: ' + item.id + ' ?');
+      }
+    }
+  ];
+
+  $scope.onItemDelete = function (item) {
+    $scope.items.splice($scope.items.indexOf(item), 1);
+  };
+
+  $scope.items = [
+    {
+      id: 1
+    },
+    {
+      id: 2
+    },
+    {
+      id: 3
+    },
+    {
+      id: 4
+    },
+    {
+      id: 5
+    },
+    {
+      id: 6
+    },
+    {
+      id: 7
+    },
+    {
+      id: 8
+    },
+    {
+      id: 9
+    },
+    {
+      id: 10
+    }
+  ];
+
+})
+
+.controller('ButtonsTabCtrl', function ($scope, $ionicPopup, $ionicActionSheet, $ionicModal) {
+    $scope.showPopup = function () {
+     $ionicPopup.alert({
+       title: 'Popup',
+       content: 'This is ionic popup alert!'
+     });
+    };
+    $scope.showActionsheet = function () {
+        $ionicActionSheet.show({
+          titleText: 'Ionic ActionSheet',
+          buttons: [
+            {
+              text: 'Facebook'
+            },
+            {
+              text: 'Twitter'
+            },
+          ],
+          destructiveText: 'Delete',
+          cancelText: 'Cancel',
+          cancel: function () {
+            console.log('CANCELLED');
+          },
+          buttonClicked: function (index) {
+            console.log('BUTTON CLICKED', index);
+            return true;
+          },
+          destructiveButtonClicked: function () {
+            console.log('DESTRUCT');
+            return true;
+          }
+        });
+    };
+    $ionicModal.fromTemplateUrl('modal.html', function (modal) {
+        $scope.modal = modal;
+      }, {
+        animation: 'slide-in-up'
+      });
+})
+
+.controller('SlideboxCtrl', function($scope, $ionicSlideBoxDelegate) {
+  $scope.nextSlide = function() {
+    $ionicSlideBoxDelegate.next();
+  }             
+})              
+
+.controller('MenuCtrl', function($scope, $ionicSideMenuDelegate, $ionicModal) {
+  $scope.toggleLeft = function() {
+    $ionicSideMenuDelegate.toggleLeft();
+  };
+              
+  $ionicModal.fromTemplateUrl('modal.html', function (modal) {
+    $scope.modal = modal;
+  }, {
+    animation: 'slide-in-up'
+  });
+ })
+
+  
+ .controller('AppCtrl', function() {
+
+  ionic.Platform.ready(function() {
+
+  });
+
+ });
+              
+              
